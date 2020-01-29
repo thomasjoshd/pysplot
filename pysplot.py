2c69a57915f035a95a47952cacd0ba59d631bd50
thomasjoshd <thomas.joshd@gmail.com>
2020-01-28 20:09:15 -0500

    clear region (X)

--- pysplot.py
+++ pysplot.py
@@ -5,7 +5,7 @@ Tested on Python 3.6.5 and 3.6.7, Linux Mint 19.1 and Windows 10.
 Uses Astropy library, and some parts are directly modified from the UVES tutorial.
 Tested using astropy-3.2.3 numpy-1.17.4"""
 
-version="0.6.3"
+version="0.6.4"
 
 import sys
 if sys.version_info < (3, 5):
@@ -147,7 +147,7 @@ class App:
         regionmenu = tk.Menu(menu)
         menu.add_cascade(label="Region", menu=regionmenu)
         regionmenu.add_command(label="Define Region (x)", command=self.regionload)
-        regionmenu.add_command(label="Clear Region (r)", command=self.reset)
+        regionmenu.add_command(label="Clear Region (X)", command=self.region_clear)
         regionmenu.add_command(label="Equivalent Width (e)", command=self.eqw)
         regionmenu.add_command(label="Gaussian (g)", command=partial(self.fit,func="gauss"))
         regionmenu.add_command(label="Voigt (v)", command=partial(self.fit,func="voigt"))
